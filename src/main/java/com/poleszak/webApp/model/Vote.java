aa6a83cbfb50135fa2e8233773eaf7be32fb9d1e
Poleszak <poleszak@protonmail.com>
2022-01-13 18:39:17 +0100

    Vote & VoteType - model

--- src/main/java/com/poleszak/webApp/model/Vote.java
+++ src/main/java/com/poleszak/webApp/model/Vote.java
@@ -1,5 +1,23 @@
 package com.poleszak.webApp.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import static javax.persistence.FetchType.LAZY;
+
 public class Vote
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voteId;
+    private VoteType voteType;
+
+    @NotNull
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
 }
--- src/main/java/com/poleszak/webApp/model/VoteType.java
+++ src/main/java/com/poleszak/webApp/model/VoteType.java
@@ -1,5 +1,12 @@
 package com.poleszak.webApp.model;
 
-public class VoteType
+public enum VoteType
 {
+    UPVOTE(1), DOWNVOTE(-1);
+
+    private int direction;
+
+    VoteType(int direction) {
+    }
+
 }
