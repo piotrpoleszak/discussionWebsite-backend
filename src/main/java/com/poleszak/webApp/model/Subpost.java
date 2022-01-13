616054f45dcba1e1a116a2e2ad5d322519e72e00
Poleszak <poleszak@protonmail.com>
2022-01-13 18:31:15 +0100

    Subpost - model

--- src/main/java/com/poleszak/webApp/model/Subpost.java
+++ src/main/java/com/poleszak/webApp/model/Subpost.java
@@ -1,21 +1,19 @@
 package com.poleszak.webApp.model;
 
-import lombok.*;
-import org.hibernate.Hibernate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 import java.util.List;
-import java.util.Objects;
 
 import static javax.persistence.FetchType.LAZY;
 import static javax.persistence.GenerationType.IDENTITY;
 
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -33,26 +31,9 @@ public class Subpost
     private String description;
 
     @OneToMany(fetch = LAZY)
-    @ToString.Exclude
     private List<Post> posts;
     private Instant createdDate;
 
     @ManyToOne(fetch = LAZY)
-    @ToString.Exclude
     private User user;
-
-    @Override
-    public boolean equals(Object o)
-    {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Subpost subpost = (Subpost) o;
-        return id != null && Objects.equals(id, subpost.id);
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return getClass().hashCode();
-    }
 }
