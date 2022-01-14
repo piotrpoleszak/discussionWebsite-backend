530f9f04024c4f05a59ddfd3db8a6f0423a71014
Poleszak <poleszak@protonmail.com>
2022-01-14 12:15:25 +0100

    AuthService - create token

--- src/main/java/com/poleszak/webApp/service/AuthService.java
+++ src/main/java/com/poleszak/webApp/service/AuthService.java
@@ -2,20 +2,49 @@ package com.poleszak.webApp.service;
 
 import com.poleszak.webApp.dto.RegisterRequest;
 import com.poleszak.webApp.model.User;
+import com.poleszak.webApp.model.VerificationToken;
+import com.poleszak.webApp.repository.UserRepository;
+import com.poleszak.webApp.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class AuthService
 {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
     public void signup(RegisterRequest registerRequest)
     {
         User user = new User();
         user.setUsername(registerRequest.getUsername());
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setCreated(Instant.now());
         user.setEnabled(false);
+
+        userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+    }
+
+    private String  generateVerificationToken(User user)
+    {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+
+        return token;
     }
 }
