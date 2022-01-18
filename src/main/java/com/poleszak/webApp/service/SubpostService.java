f4dda735d22f9e86a2f1aefa20c6ec6ed66b5e2a
Poleszak <poleszak@protonmail.com>
2022-01-18 22:30:13 +0100

    Subpost API

--- src/main/java/com/poleszak/webApp/service/SubpostService.java
+++ src/main/java/com/poleszak/webApp/service/SubpostService.java
@@ -1,5 +1,14 @@
 package com.poleszak.webApp.service;
 
+import com.poleszak.webApp.dto.SubpostDto;
+import com.poleszak.webApp.model.Subpost;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
 public class SubpostService
 {
 }
