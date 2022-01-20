c36a2b0fa4f55c12e4a2bbdd4b827a53f848c2ad
Poleszak <poleszak@protonmail.com>
2022-01-20 19:21:42 +0100

    Subpost - change fetch for list of posts

--- src/main/java/com/poleszak/webApp/model/Subpost.java
+++ src/main/java/com/poleszak/webApp/model/Subpost.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 import java.util.List;
 
+import static javax.persistence.FetchType.EAGER;
 import static javax.persistence.FetchType.LAZY;
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -30,7 +31,7 @@ public class Subpost
     @NotBlank(message = "Description is required")
     private String description;
 
-    @OneToMany(fetch = LAZY)
+    @OneToMany(fetch = EAGER)
     private List<Post> posts;
     private Instant createdDate;
 
