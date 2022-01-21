ed94646c56be29e6b98da41c4e04adabfb88feda
Poleszak <poleszak@protonmail.com>
2022-01-21 22:06:29 +0100

    API to manage comments - add CommentController

---
+++ src/main/java/com/poleszak/webApp/controller/CommentsController.java
@@ -0,0 +1,12 @@
+package com.poleszak.webApp.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/comments/")
+@AllArgsConstructor
+public class CommentsController
+{
+}
