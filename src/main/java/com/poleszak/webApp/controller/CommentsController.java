32498fd6a7ad53e77143da3c725f6e28822796ad
Poleszak <poleszak@protonmail.com>
2022-01-21 22:08:44 +0100

    API to manage comments - add CommentsDto

--- src/main/java/com/poleszak/webApp/controller/CommentsController.java
+++ src/main/java/com/poleszak/webApp/controller/CommentsController.java
@@ -1,6 +1,8 @@
 package com.poleszak.webApp.controller;
 
+import com.poleszak.webApp.dto.CommentsDto;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class CommentsController
 {
+    @PostMapping
+    public void createComment(@RequestMapping CommentsDto commentsDto)
+    {
+
+    }
 }
