c2730ac07b88a90909e28dba2c77ab44b99e62db
Poleszak <poleszak@protonmail.com>
2022-01-21 22:14:52 +0100

    API to manage comments

--- src/main/java/com/poleszak/webApp/controller/CommentsController.java
+++ src/main/java/com/poleszak/webApp/controller/CommentsController.java
@@ -3,6 +3,7 @@ package com.poleszak.webApp.controller;
 import com.poleszak.webApp.dto.CommentsDto;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentsController
 {
     @PostMapping
-    public void createComment(@RequestMapping CommentsDto commentsDto)
+    public void createComment(@RequestBody CommentsDto commentsDto)
     {
 
     }
---
+++ src/main/java/com/poleszak/webApp/service/CommentService.java
@@ -0,0 +1,26 @@
+package com.poleszak.webApp.service;
+
+import com.poleszak.webApp.dto.CommentsDto;
+import com.poleszak.webApp.exceptions.PostNotFoundException;
+import com.poleszak.webApp.model.Post;
+import com.poleszak.webApp.repository.PostRepository;
+import com.poleszak.webApp.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CommentService
+{
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final AuthService authService;
+
+    public void save(CommentsDto commentsDto)
+    {
+        Post post = postRepository.findById(commentsDto.getPostId())
+                        .orElseThrow(() -> new PostNotFoundException(commentsDto.getPostId().toString()));
+
+        
+    }
+}
