16d7f739605008e79d5acc11f836e2c7017bc1ef
Poleszak <poleszak@protonmail.com>
2022-01-23 23:01:11 +0100

    Comment API - CommentService

--- src/main/java/com/poleszak/webApp/service/CommentService.java
+++ src/main/java/com/poleszak/webApp/service/CommentService.java
@@ -4,7 +4,10 @@ import com.poleszak.webApp.dto.CommentsDto;
 import com.poleszak.webApp.exceptions.PostNotFoundException;
 import com.poleszak.webApp.mapper.CommentMapper;
 import com.poleszak.webApp.model.Comment;
+import com.poleszak.webApp.model.NotificationEmail;
 import com.poleszak.webApp.model.Post;
+import com.poleszak.webApp.model.User;
+import com.poleszak.webApp.repository.CommentRepository;
 import com.poleszak.webApp.repository.PostRepository;
 import com.poleszak.webApp.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -14,10 +17,14 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class CommentService
 {
+    private static final String POST_URL = "";
     private final PostRepository postRepository;
     private final UserRepository userRepository;
     private final AuthService authService;
     private final CommentMapper commentMapper;
+    private final CommentRepository commentRepository;
+    private final MailContentBuilder mailContentBuilder;
+    private final MailService mailService;
 
     public void save(CommentsDto commentsDto)
     {
@@ -25,5 +32,14 @@ public class CommentService
                         .orElseThrow(() -> new PostNotFoundException(commentsDto.getPostId().toString()));
 
         Comment comment = commentMapper.map(commentsDto, post, authService.getCurrentUser());
+        commentRepository.save(comment);
+
+        String message = mailContentBuilder.build(post.getUser().getUsername() + " posted a comment on your post." + POST_URL);
+        sendCommentNotification(message, post.getUser());
+    }
+
+    private void sendCommentNotification(String message, User user)
+    {
+        mailService.sendMail(new NotificationEmail(user.getUsername() + "Commented on your post", user.getEmail(), message));
     }
 }
