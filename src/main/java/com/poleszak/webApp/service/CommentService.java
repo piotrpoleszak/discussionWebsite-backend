329ee5fe8c3bfe60ee6d95904fc8a0378ac9303b
Poleszak <poleszak@protonmail.com>
2022-01-23 22:54:53 +0100

    Implement CommentMapper in CommentService

--- src/main/java/com/poleszak/webApp/service/CommentService.java
+++ src/main/java/com/poleszak/webApp/service/CommentService.java
@@ -2,6 +2,8 @@ package com.poleszak.webApp.service;
 
 import com.poleszak.webApp.dto.CommentsDto;
 import com.poleszak.webApp.exceptions.PostNotFoundException;
+import com.poleszak.webApp.mapper.CommentMapper;
+import com.poleszak.webApp.model.Comment;
 import com.poleszak.webApp.model.Post;
 import com.poleszak.webApp.repository.PostRepository;
 import com.poleszak.webApp.repository.UserRepository;
@@ -15,12 +17,13 @@ public class CommentService
     private final PostRepository postRepository;
     private final UserRepository userRepository;
     private final AuthService authService;
+    private final CommentMapper commentMapper;
 
     public void save(CommentsDto commentsDto)
     {
         Post post = postRepository.findById(commentsDto.getPostId())
                         .orElseThrow(() -> new PostNotFoundException(commentsDto.getPostId().toString()));
 
-
+        Comment map = commentMapper.map(commentsDto, post, authService.getCurrentUser());
     }
 }
