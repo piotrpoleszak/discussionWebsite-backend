694ae8977d9f964403a0096d326862cade015ea5
Poleszak <poleszak@protonmail.com>
2022-01-24 22:03:12 +0100

    VoteDto

--- src/main/java/com/poleszak/webApp/controller/VoteController.java
+++ src/main/java/com/poleszak/webApp/controller/VoteController.java
@@ -1,7 +1,12 @@
 package com.poleszak.webApp.controller;
 
+import com.poleszak.webApp.dto.VoteDto;
+import com.poleszak.webApp.model.Vote;
 import com.poleszak.webApp.service.VoteSerive;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class VoteController
 {
     private final VoteSerive voteSerive;
+
+    @PostMapping
+    public ResponseEntity<Vote> vote(@RequestBody VoteDto voteDto)
 }
---
+++ src/main/java/com/poleszak/webApp/dto/VoteDto.java
@@ -0,0 +1,5 @@
+package com.poleszak.webApp.dto;
+
+public class VoteDto
+{
+}
