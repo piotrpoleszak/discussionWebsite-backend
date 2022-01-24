c35fb1117909dd88fd6dcb1e4cc442ab758cf1b9
Poleszak <poleszak@protonmail.com>
2022-01-24 22:02:16 +0100

    VoteService

--- src/main/java/com/poleszak/webApp/controller/VoteController.java
+++ src/main/java/com/poleszak/webApp/controller/VoteController.java
@@ -1,5 +1,14 @@
 package com.poleszak.webApp.controller;
 
+import com.poleszak.webApp.service.VoteSerive;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/votes/")
+@AllArgsConstructor
 public class VoteController
 {
+    private final VoteSerive voteSerive;
 }
