8db48aefaaf8ffde7b642b65779c486c228d0a00
Poleszak <poleszak@protonmail.com>
2022-01-25 11:22:52 +0100

    Manage Votes

--- src/main/java/com/poleszak/webApp/model/VoteType.java
+++ src/main/java/com/poleszak/webApp/model/VoteType.java
@@ -1,5 +1,9 @@
 package com.poleszak.webApp.model;
 
+import com.poleszak.webApp.exceptions.SpringDiscussionwebsiteException;
+
+import java.util.Arrays;
+
 public enum VoteType
 {
     UPVOTE(1), DOWNVOTE(-1);
@@ -7,5 +11,18 @@ public enum VoteType
     private int direction;
 
     VoteType(int direction) {}
+
+    public static VoteType lookup(Integer direction)
+    {
+        return Arrays.stream(VoteType.values())
+                .filter(value -> value.getDirection().equals(direction))
+                .findAny()
+                .orElseThrow(() -> new SpringDiscussionwebsiteException("Vote not found"));
+    }
+
+    public Integer getDirection()
+    {
+        return direction;
+    }
 
 }
